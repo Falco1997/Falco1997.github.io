8c6c6c0af0ae8bd30edbd760392fa1dbc9d1d570
Falco1997 <filipgolf1997@gmail.com>
2024-06-08 18:47:13 +0200

    chap21

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,50 @@
 <script>
 	import Button from './lib/Button.svelte';
+	import TodoList from './lib/TodoList.svelte';
+	import FaAccessibleIcon from 'svelte-icons/fa/FaAccessibleIcon.svelte';
+	import FaAd from 'svelte-icons/fa/FaAd.svelte';
+	import { v4 as uuid } from 'uuid';
+
+	const todos = [
+		{
+			id: uuid(),
+			title: 'Todo 1',
+			completed: true
+		},
+		{
+			id: uuid(),
+			title: 'Todo 2',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Todo 3',
+			completed: true
+		}
+	];
 </script>
 
-<Button size="small" shadow>Search</Button>
+<Button
+	on:click|once={(event) => {
+		alert(true);
+	}}
+	size="small"
+	shadow
+	bgColor="orange"
+	textColor="black"
+	disabled
+>
+	<div let:isLeftHovered style:width="20px" slot="leftContent">
+		{#if isLeftHovered}
+			<FaAccessibleIcon />
+		{:else}
+			<FaAd />
+		{/if}
+	</div>
+	Search
+</Button>
+
+<TodoList {todos} />
 
 <style>
 </style>
--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,22 +1,60 @@
 <script>
 	export let size = 'small';
 	export let shadow = false;
+	export let bgColor = 'inherit';
+	export let textColor = 'inherit';
+	//export let disabled = true;
+	console.log($$restProps);
+	let isLeftHovered;
 </script>
 
 <button
+	on:click
+	style:--buttonBgColor={bgColor}
+	style:--buttonTextColor={textColor}
 	class:size-sm={size === 'small'}
 	class:size-md={size === 'medium'}
-	class:size-lg={size === 'large'}><slot>Fallback Search</slot></button
+	class:size-lg={size === 'large'}
+	class:has-left={$$slots.leftContent}
+	class:shadow
+	{...$$restProps}
 >
+	{#if $$slots.leftContent}
+		<div
+			class="left-content"
+			on:mouseenter={() => (isLeftHovered = true)}
+			on:mouseenter={() => console.log('mouseentered')}
+			on:mouseleave={() => (isLeftHovered = false)}
+			on:mouseleave={() => console.log('mouseleave')}
+			role="none"
+		>
+			<slot name="leftContent" {isLeftHovered} />
+		</div>
+	{/if}
+
+	<slot>Fallback Search</slot>
+</button>
 
 <style lang="scss">
 	button {
+		display: flex;
+		align-items: center;
 		border: none;
-		background-color: orange;
-		color: black;
+		background-color: var(--buttonBgColor);
+		color: var(--buttonTextColor);
 		font-weight: bold;
 		border-radius: 5px;
 		cursor: pointer;
+		.left-content {
+			margin-right: 10px;
+		}
+		&:disabled {
+			opacity: 0.5;
+			cursor: not-allowed;
+		}
+		&:hover {
+			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+		}
 		&.size-sm {
 			padding: 15px 20px;
 			font-size: 16px;
@@ -28,6 +66,9 @@
 		&.size-lg {
 			padding: 35px 40px;
 			font-size: 32px;
+		}
+		&.shadow {
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
 		}
 	}
 </style>
